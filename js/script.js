902a9225e949d1e634055c854724c38abf682aea
Maximiliano Fernández <mafernandez@miem.local>
2019-06-22 18:23:18 -0300

    Optimización de SVG. Arreglo en línea de script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Verificar si hay un hash en la locación
-if (window.location.hash.length <= 0)
+if (window.location.hash.slice(1).length <= 0)
 {
 	// No hay referencia a una sección. Ponerle
 	window.location = '#home';
